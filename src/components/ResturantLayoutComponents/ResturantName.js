781bee7d0488d8cd79bab413e5e7a4c3ba6956e2
sindhu-149 <sindhukanakarao149@gmail.com>
2023-01-13 22:29:08 +0530

    upadate 3

--- src/components/ResturantLayoutComponents/ResturantName.js
+++ src/components/ResturantLayoutComponents/ResturantName.js
@@ -10,26 +10,17 @@ const ResturantName = () => {
      <h1>The Pizza Store</h1>
      <img  style={{ width:"30px", height:"30px", margin:"-6px 15px"}} src="https://cdn-icons-png.flaticon.com/512/854/854878.png"></img>
      <p style={{ display:"inline"}}>21 A, Street Ram Nagar, Hyderabda</p>
-     <div 
-     style={{
-      marginTop:"40px",
-     }}
-     >
-   
-      {types.map(type =>(
+     <div  className='title-bg-type'>
+   {types.map(type =>(
         <span 
-        style={{
-          margin:"0px 10px",
-          padding:"10px",
-       
-          borderRadius:"5px",
-          backgroundColor: "#ECECEC"
-        }}
-         
+        className='title-bg-type-test'
+        
         >{type}</span>
       ))}
+   </div>
+     
     </div>
-    </div>
+    {/* </div> */}
     <div className='resturant-name'>
       <img className='resturant-name-image' src="https://charatoon.com/photo/thum/6342.png"></img>
     </div>
