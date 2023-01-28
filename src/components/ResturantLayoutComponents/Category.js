b13f4bc0a159964d378699af034f0e967fd7d7db
sindhu-149 <sindhukanakarao149@gmail.com>
2023-01-28 16:26:45 +0530

    upadate 14

--- src/components/ResturantLayoutComponents/Category.js
+++ src/components/ResturantLayoutComponents/Category.js
@@ -7,7 +7,7 @@ const Category = (props) => {
   // let params =useParams()
   // console.log(params);
 
-  const {data, onAdd} = props;
+  const {data, onAdd,className,onClick,key} = props;
 
   const [text,setText] = useState("none")
   const [toggle, setToggle] = useState("false")
@@ -28,11 +28,19 @@ const Category = (props) => {
   return (
     
     <div  className= "category">
-     <div style={{ display:"flex", justifyContent:"space-between", padding:"8px",cursor:"pointer"}}  className= "category" onClick={handleClick}>
+     <div  style={{ display:"flex", justifyContent:"space-between", padding:"8px",cursor:"pointer"}}  className="category"  key={key} onClick={onClick}
+     value={props.name}
+    //  onClick={handleClick}
+    //  onClick ={hello}
+     >
       <p style={{border:"0px solid red",textAlign:"start", padding:"5px 10px", margin:"0px"}}> {props.name}</p>
       <img  style={{border:"0px solid red",width:"30px",height:"30px",padding:"0px 10px"}} src='https://cdn-icons-png.flaticon.com/512/7996/7996254.png'></img>
       </div>
-      <p  className='menucards-list' style={{display: text}}>
+      <p  
+      className='menucards-list'
+      // className= {className}
+      // onClick={onClick}
+       style={{display: className}}>
       <MenuCards onAdd={onAdd} data={data} name = {props.name}/>
        
        </p>
--- src/components/ResturantLayoutComponents/Menu.js
+++ src/components/ResturantLayoutComponents/Menu.js
@@ -2,64 +2,63 @@ import React, {useState} from 'react'
 
 import Category from './Category'
 
-// import numberOfCategory from "../../WOTM_TheRedBox_Gajuwaka.json" 
-
-// let categories = numberOfCategory.map( ({Category}) => ({Category}))
-
-// let cata = categories.map(JSON.stringify)
-
-// cata = new Set(cata) 
-
-// cata = Array.from(cata).map(JSON.parse)
-
-// console.log(cata);
-
-
-
- 
 const Menu = (props) => {
-  
   const {data, onAdd} = props
-
-  const [wrap, setWrap] = useState("2")
-  const [toogle,setToggle] = useState("false")
-
-function handleClick(){
-  if (toogle === "false"){
-    setWrap("7")
-    setToggle("true")
-  }
-  else{
-    setWrap("2")
-    setToggle("false")
-  }
-}
-  
-// let result = numberOfCategory.map(a => a.Category)
-let result = data.map(a => a.Category)
+  let result = data.map(a => a.Category)
 result = new Set(result) 
 result = Array.from(result)
+  const [appState, changeState] = useState({
+    activeObject : null,
+    objects:result
+  });
 
-// console.log(result); 
+
+  function toggleActive(index){
+  if(appState.objects[index] === appState.activeObject){
+    changeState({...appState, activeObject: null});
+  }else{
+    changeState({...appState, activeObject: appState.objects[index]});
+  }
+}
 
 
-  return (
+  function toggleStyle(index){
+    if(appState.objects[index] === appState.activeObject){
+      return "block";
+    }
+    else{
+      return "none";
+    }
+  }
+  return(
+    // <div>
+    //   {appState.objects.map((elements,index) =>(
+    //     <div className={toggleStyle(index)} key={index} onClick={ () => toggleActive(index)}>
+    //     </div>
+    //    ))}
+
+  
+    // </div>
     <div className='menu'> 
 
-<div className='head'>
+    <div className='head'>
+    
+           <p className='head-name'>Menu</p>
 
-      <p className='head-name'>Menu</p>
-      {/* <p 
-      onClick={handleClick}
-      className='view-btn' 
-      >view all</p>  */}
-    </div>
-    {result.map(cat =>(
-      <Category  onAdd={onAdd} data={data} name={cat} />
-      
-    ))}
-    </div>
+         </div>
+         {appState.objects.map((elements,index) =>(
+          <div>
+        {/* <div className={toggleStyle(index)} key={index} onClick={ () => toggleActive(index)}> */}
+        <Category className={toggleStyle(index)} key={index} onClick={ () => toggleActive(index)} onAdd={onAdd} data={data} name={elements} />
+        </div>
+       ))}
+         {/* {result.map(cat =>(
+           <Category  onAdd={onAdd} data={data} name={cat} />
+          
+         ))} */}
+         </div>
   )
 }
+
 
 export default Menu
