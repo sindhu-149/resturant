3c920a5dce9315f845d370b62efd54cb21b98743
sindhu-149 <sindhukanakarao149@gmail.com>
2023-01-30 20:52:38 +0530

    upadate 15

--- src/components/ResturantLayoutComponents/Navbar.js
+++ src/components/ResturantLayoutComponents/Navbar.js
@@ -3,7 +3,16 @@ import ResturantName from './ResturantName'
 const Navbar = (props) => {
 const {res}=props
   return (
-    <div className='navbar'style={{ backgroundImage: `url(${res.Background})`}}>
+    <div className='navbar'
+    // style={{ backgroundImage: `url(${res.Background})`}}
+    >
+    {/* <img
+    style={{
+      height: "100",
+  width: "100%",
+  objectit: cover;
+    }}
+     src={res.Background}></img> */}
     <ResturantName  data={res}/>
     </div>
   )
