970620faa7f9a273c112a588ec911bc053bb458f
sindhu-149 <sindhukanakarao149@gmail.com>
2023-02-07 23:42:20 +0530

    upadate 22

--- src/components/HomeComponents/Content.js
+++ src/components/HomeComponents/Content.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import scan from '../../images/scan-image.png'
 import adaptable from '../../images/adaptable.png'
 import contact from '../../images/contact.png'
 import convenient from '../../images/convenient.png'
 import customize from '../../images/customize.png'
 const Content = () => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function handleResize() {
+          setWindowWidth(window.innerWidth);
+        }
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
 
     return (
         <div
@@ -16,17 +27,15 @@ const Content = () => {
                 padding: "3%"
             }}>
 
-                <div className='box1'
-                >
-                    <p className='box1-text'
-                      >Scan & Savor</p>
-                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+                <div className='box1'>
+                    <p className='box1-text'>Scan & Savor </p>
+                    <p className='box1-text2'>Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
                     <button className='btn'>Call to action</button>
                 </div>
 
                 <div >
                     <div>
-                        <p
+                        <p 
                             className='contact-img box1-img'>
                             <img
                                 className='contact-img'
@@ -42,14 +51,12 @@ const Content = () => {
                 <div className='content-bg'>
                     <div
                      className='box2'>
-                        <p className='box2-text'>Adaptable</p>
+                        <p className='box2-text'><span className='box2-text-sub'>Adaptable</span></p>
                         <p className='box2-text2'>for every Resturant</p>
-                        <button
-                          className='btn'>Call to action</button>
+                        <p className='box2-btn'> <button className='btn'>Call to action</button></p>
                     </div>
 
-                    <div
-                      >
+                    <div className='adaptable'>
                         <img className='adaptable-img' src={adaptable}></img>
                     </div>
                 </div>
@@ -68,32 +75,51 @@ const Content = () => {
                     <div
                      className='box3'>
                         <p className='box3-text'>Contactless Menu</p>
-                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+                        <p className='box1-text2'>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
                         <button className='btn'>Call to action</button>
                     </div>
 
-                    <div>
+                    <div className='contact'>
                         <img className='contact-img' src={contact}></img>
                     </div>
 
                 </div>
 
             </div>
-            <div
-                className='home-content box4'
-              >
-                <div >
+            <div>
+                {windowWidth < 680 ?
+                
+                <div className='home-content box4'>
+
+                <div className='box4-text'>
+                    <p className='box3-text'>convenient for customers</p>
+                    <p className='box1-text2'>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+                    <button className='btn'>Call to action</button>
+                </div>
+
+                <div className='convenient'>
+                    <img className='contact-img' src={convenient}></img>
+                </div>
+
+                </div>
+                 : 
+
+                  <div className='home-content box4'>
+                <div className='convenient'>
                     <img className='contact-img' src={convenient}></img>
                 </div>
 
                 <div className='box4-text'>
-                    <p
-                        className='box3-text'>convenient for customers</p>
-                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+                    <p className='box3-text'>convenient for customers</p>
+                    <p className='box1-text2'>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
                     <button className='btn'>Call to action</button>
                 </div>
 
             </div>
+
+                }
+            </div>
+            
             <div
                 className='home-content'
                 style={{
@@ -102,11 +128,11 @@ const Content = () => {
 
                 <div className='box3'>
                     <p className='box3-text'>Contactless Menu</p>
-                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+                    <p className='box1-text2'>Contrary to popular belief, Lorem Ipsum is not simply random text. </p>
                     <button className='btn'>Call to action</button>
                 </div>
 
-                <div >
+                <div className='convenient'>
                     <img className='contact-img' src={customize}></img>
                 </div>
 
